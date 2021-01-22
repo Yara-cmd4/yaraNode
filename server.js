79713d15490c37dbecaae76b647f4c2ee0579195
يارا كبها <yara.kbha4@gmail.com>
2021-01-22 12:45:34 +0200

    hi

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require('express')
 const app = express();
 
-const port = process.env.PORT || 3000;
-app.use(express.static('public'))
 
-app.listen(port, function () {
-    console.log("listen")
+app.use(express.static('public'));
+
+const users = [
+  {name:'Kilani', id:'222222222'},
+  {name:'Yara', id:'333333333'},
+  {name:'Ihab', id:'444444'}
+]
+
+//route
+app.get('/get-users',(req, res)=>{
+  res.send(users)
 })
+
+
+
+
+const port = process.env.PORT || 3000;
